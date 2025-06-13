39d9d85125d542eb923b5aa6dbd1ffba59c0abde
Giouxel Kasaika <uxell@Mac.fritz.box>
2025-06-13 14:05:57 +0200

    feature add post , save to data

--- app.py
+++ app.py
@@ -1,16 +1,41 @@
+from os import WCONTINUED
+
 from flask import Flask, render_template, request, redirect, url_for
 import json
+
+
 
 app = Flask(__name__)
 
 data_path = "data/blog_posts.json"
 
-def load_data(data_path):
 
-    with open(data_path, "r") as json_data:
+
+def load_data(path):
+
+    with open(path, "r") as json_data:
         content = json_data.read()
         data = json.loads(content)
         return data
+
+def save_post(author, title, content):
+    existing_posts = load_data(data_path)
+
+    user_ids = {post["id"] for post in existing_posts}
+    free_id = 0
+    while free_id in user_ids:
+        free_id += 1
+
+
+    new_posts = { "id" : free_id,
+                 "author": author,
+                 "title": title,
+                 "content": content
+    }
+    existing_posts.append(new_posts)
+
+    with open(data_path, "w") as new_posts_data:
+        json.dump(existing_posts, new_posts_data, indent=4 )
 
 @app.route('/')
 def index():
@@ -25,9 +50,10 @@ def add():
         author = request.form.get('author', 'NoAuthor')
         title = request.form.get('title', 'NoTitle')
         content = request.form.get('content', 'NoContent')
+        save_post(author, title, content)
         return redirect(url_for('index'))
 
     return render_template('add.html')
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="127.0.0.1", port=5001, debug=True)
