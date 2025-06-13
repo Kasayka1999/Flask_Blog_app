fb3f8f7ee89369a760e77d6d67b9017fe0ca3724
Giouxel Kasaika <uxell@Mac.fritz.box>
2025-06-13 13:20:09 +0200

    fixed index.html to show posts, added form for add_post

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import json
 
 app = Flask(__name__)
 
-def load_data():
-    data_path = "data/blog_posts.json"
+data_path = "data/blog_posts.json"
+
+def load_data(data_path):
 
     with open(data_path, "r") as json_data:
         content = json_data.read()
@@ -13,10 +14,20 @@ def load_data():
 
 @app.route('/')
 def index():
-    blog_posts = load_data()
+    blog_posts = load_data(data_path)
     # add code here to fetch the job posts from a file
     return render_template('index.html', posts=blog_posts)
 
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        author = request.form.get('author', 'NoAuthor')
+        title = request.form.get('title', 'NoTitle')
+        content = request.form.get('content', 'NoContent')
+        return redirect(url_for('index'))
+
+    return render_template('add.html')
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=5000, debug=True)
