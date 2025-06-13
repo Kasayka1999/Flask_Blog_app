990472eae328f4c15af41000ee8f8692b05f18bf
Giouxel Kasaika <uxell@Mac.fritz.box>
2025-06-13 12:56:15 +0200

    first structure datajson/html/css

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
+def load_data():
+    data_path = "data/blog_posts.json"
+
+    with open(data_path, "r") as json_data:
+        content = json_data.read()
+        data = json.loads(content)
+        return data
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    blog_posts = load_data()
+    # add code here to fetch the job posts from a file
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
