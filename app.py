bdc3e72823425298234622d698350e4556f5a798
Giouxel Kasaika <uxell@Mac.fritz.box>
2025-06-13 15:59:20 +0200

    added update post feature and button after each post

--- app.py
+++ app.py
@@ -111,4 +111,4 @@ def update(post_id):
     return render_template('update.html', post=post)
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=5001, debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=True)
